0a1d611df19387fbaa6b194e2f50c3342f15a7b7
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-19 09:31:16 +0200

    Removing native JPA implementation

--- src/main/java/com/orm/querybenchmark/repos/ActorRepository.java
+++ src/main/java/com/orm/querybenchmark/repos/ActorRepository.java
@@ -8,13 +8,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
-    @Query(nativeQuery = true, value = "SELECT * FROM public.actor")
-    List<Actor> nativeFindAll();
-
-    @Query(nativeQuery = true, value = "SELECT * FROM public.actor " +
-            "where public.actor.actor_id = :idParam")
-    Actor nativeFindById(@Param("idParam") Integer id);
-
-    @Query(nativeQuery = true, value = "INSERT INTO public.actor VALUES(:idParam, :firstNameParam, :lastNameParam)")
-    void nativeSave(@Param("idParam") Integer id,@Param("firstNameParam") String firstName,@Param("lastNameParam") String lastName);
 }
