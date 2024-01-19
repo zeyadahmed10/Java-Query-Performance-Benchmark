bcab7d9f643e4e714bff189c2ba826ead5196b2c
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-19 21:52:28 +0200

    implementing findAllByReleaseYear function in ActorDaoJOOQImpl.java

--- src/main/java/com/orm/querybenchmark/dao/ActorDaoJOOQImpl.java
+++ src/main/java/com/orm/querybenchmark/dao/ActorDaoJOOQImpl.java
@@ -2,12 +2,16 @@ package com.orm.querybenchmark.dao;
 
 import com.orm.querybenchmark.entity.Actor;
 import static org.jooq.generated.public_.tables.Actor.ACTOR;
+import static org.jooq.generated.public_.tables.Film.FILM;
+import static org.jooq.generated.public_.tables.FilmActor.FILM_ACTOR;
 
 import com.orm.querybenchmark.mappers.ActorRowMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.jooq.DSLContext;
+import org.jooq.SelectConditionStep;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -55,6 +59,16 @@ public class ActorDaoJOOQImpl implements ActorDAO{
 
     @Override
     public List<Actor> findAllByFilmReleaseYear(Integer year) {
-        return null;
+        var records= dslContext
+                .selectDistinct(ACTOR.ACTOR_ID, ACTOR.FIRST_NAME, ACTOR.LAST_NAME)
+                .from(ACTOR)
+                .join(FILM_ACTOR).on(ACTOR.ACTOR_ID.eq(FILM_ACTOR.ACTOR_ID))
+                .join(FILM).on(FILM_ACTOR.FILM_ID.eq(FILM.FILM_ID))
+                .and(FILM.RELEASE_YEAR.eq(year)).fetch();
+        if(records==null){
+            log.error("No records found with year: "+year);
+            return null;
+        }
+        return ActorRowMapper.mapActorRecord(records);
     }
 }
