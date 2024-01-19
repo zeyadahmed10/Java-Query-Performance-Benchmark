f4c7719d4e6d9b76f6bbd7ef37ea20f61e919225
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-01-19 21:14:34 +0200

    Adding mapping to map records to actor class

--- src/main/java/com/orm/querybenchmark/mappers/ActorRowMapper.java
+++ src/main/java/com/orm/querybenchmark/mappers/ActorRowMapper.java
@@ -1,10 +1,16 @@
 package com.orm.querybenchmark.mappers;
 
 import com.orm.querybenchmark.entity.Actor;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.jooq.generated.public_.tables.Actor.ACTOR;
 
 public class ActorRowMapper implements RowMapper<Actor> {
 
@@ -15,5 +21,19 @@ public class ActorRowMapper implements RowMapper<Actor> {
         actor.setFirstName(rs.getString("first_name"));
         actor.setLastName(rs.getString("last_name"));
         return actor;
+    }
+    public static Actor mapActorRecord(Record record){
+        return Actor.builder().id(record.get(ACTOR.ACTOR_ID))
+                .firstName(record.get(ACTOR.FIRST_NAME))
+                .lastName(record.get(ACTOR.LAST_NAME)).build();
+    }
+    public static List<Actor> mapActorRecord(Result<Record> records){
+        List<Actor> actors = new ArrayList<Actor>();
+        for(var record: records){
+            actors.add(Actor.builder().id(record.get(ACTOR.ACTOR_ID))
+                    .firstName(record.get(ACTOR.FIRST_NAME))
+                    .lastName(record.get(ACTOR.LAST_NAME)).build());
+        }
+        return actors;
     }
 }
